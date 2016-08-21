f8a587e5b562d9dba365be28981476d0bb677d2f
HaddonFisher <HaddonFisher@gmail.com>
2016-08-21 19:34:26 -0400

    Basic Proof of Concept

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+
+<html lang="en">
+	<head>
+		<title>Proof of Concept</title>
+	</head>
+	<body>
+		<?php echo "hello world"; ?>
+	 </body>
+
+<!-- 
+* Release Notes
+* 1.0 - Let there be light
+-->
+
+</html>
