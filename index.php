a676322fbf4a6a6b559603f5fe8a6895323fafbb
HaddonFisher <HaddonFisher@gmail.com>
2016-08-21 22:35:20 -0400

    Fixed medal results display

--- index.php
+++ index.php
@@ -9,22 +9,21 @@
 		<?php echo "Hey World!";?><br>
 		<?php echo "Check out this thing I sent you!";?><br><br>
 		<?php
-		
+		//error_reporting(E_ALL & ~E_NOTICE);
 		$country = 'united-states';
 		$jsonURL = "http://www.medalbot.com/api/v1/medals/";
 
 		$fulljsonURL = $jsonURL . $country;
 		$json = file_get_contents($fulljsonURL);
-		$medal_array = json_decode($json);
+		$medal_array = json_decode($json,true);
 		?>
 
-		<?php print_r($medal_array); ?>
-		<?php echo "You requested a Rio medal count for:" . $medal_array['country_name'];?>
-		<?php echo "Bronze:" . $medal_array['bronze_count'];?>
-		<?php echo "Silver:" . $medal_array['silver_count'];?>
-		<?php echo "Gold:" . $medal_array['gold_count'];?>
+		<!--<?php print_r($medal_array); ?>-->
+		<?php echo "You requested a Rio medal count for:" . $medal_array['country_name'];?><br>
+		<?php echo "Bronze:" . $medal_array['bronze_count'];?><br>
+		<?php echo "Silver:" . $medal_array['silver_count'];?><br>
+		<?php echo "Gold:" . $medal_array['gold_count'];?><br>
 
-		?>
 		</br></br>
 		<em>Brought to you by Haddon Industries</em>
 	 </body>
